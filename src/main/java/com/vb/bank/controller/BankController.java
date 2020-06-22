4941648f1cbe806af449dfea03d61ee736b02d13
markchen20190628 <1004185602@qq.com>
2020-06-22 14:04:43 +0800

    add default bank

--- src/main/java/com/vb/bank/controller/BankController.java
+++ src/main/java/com/vb/bank/controller/BankController.java
@@ -16,9 +16,11 @@ public class BankController {
     @GetMapping(path="/bank/all")
     public Map<String, String> getBankInfoMap(){
         log.info("Receive a request to get all bank list");
+        System.out.println("System.out : Receive a request to get all bank list");
         Map<String, String> map = new HashMap<>();
         map.put("012", "China Bank Hong Kong");
         map.put("390", "Welab Bank");
+        map.put("000", "Default");
         return map;
     }
 }
