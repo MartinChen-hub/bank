f02ccbc8a6acae063904cf5c2e68ebbd1cad07be
markchen20190628 <1004185602@qq.com>
2020-06-22 11:48:49 +0800

    add log

--- src/main/java/com/vb/bank/controller/BankController.java
+++ src/main/java/com/vb/bank/controller/BankController.java
@@ -1,5 +1,7 @@
 package com.vb.bank.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,11 @@ import java.util.Map;
 @RestController
 public class BankController {
 
+    Logger log = LoggerFactory.getLogger(BankController.class);
+
     @GetMapping(path="/bank/all")
     public Map<String, String> getBankInfoMap(){
+        log.info("Receive a request to get all bank list");
         Map<String, String> map = new HashMap<>();
         map.put("012", "China Bank Hong Kong");
         map.put("390", "Welab Bank");
